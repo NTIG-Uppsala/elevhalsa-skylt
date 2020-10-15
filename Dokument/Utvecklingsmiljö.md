fe0fe57776d6446c2a7a98302fab04a42d86c74f
Daniel Hajjar <daniel.hajjar@learnet.se>
2020-10-15 11:12:01 +0200

    Added screenshot test, documented dev-env setup

---
+++ Dokument/Utvecklingsmiljö.md
@@ -0,0 +1,12 @@
+# Utvecklingsmiljö
+## Setup (Windows)
+1) Installera Python
+2) Installera webbläsare (Chrome/Chromium)
+3) Verifiera att webbläsaren är uppdaterad
+4) Ladda ner den senaste versionen av chromedriver: http://chromedriver.chromium.org/downloads
+5) Extrahera filen från den nedladdade zip-filen till Test-mappen
+6) Öppna `elevhalsa-digital-skylt.sln` i Visual Studio
+7) Installera Python-tillägget till Visual Studio
+
+## Kör skärmbildstest
+Kör Test-projektet i Visual Studio
--- Test/Test.py
+++ Test/Test.py
@@ -1,1 +1,50 @@
+import sys
+import os
+import time
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+# Bredden av webbläsaren
+WIDTH = 1366
+
+# Höjden av webbläsaren
+HEIGHT = 768
+
+chrome_options = Options()
+
+# Lägger till ett argument för att webbläsaren inte ska ha ett fönster
+chrome_options.add_argument("--headless")
+
+driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.get("{}/../Skylt/index.html".format(os.getcwd()))
+
+driver.set_window_size(1366, 768)
+
+carousel = driver.find_element_by_class_name("carousel")
+
+# Räknar ut hur länge som sidan stannar på en slide
+slide_interval = int(carousel.get_attribute("data-interval")) / 1000
+
+# Specifierar antalet slides på sidan
+slide_count = len(carousel.find_elements_by_xpath("ol/li"))
+
+# Specifierar hur länge som programmet ska försöka hitta element på sidan
+driver.implicitly_wait(slide_interval)
+
+# Skapar en mapp för screenshots om den inte redan existerar
+if not os.path.exists("screenshots"):
+	os.mkdir("screenshots")
+
+for i in range(slide_count):
+	# Försöker hitta slide med klassen "active"
+	carousel.find_element_by_xpath("ol/li[@data-slide-to='{}'][@class='{}']".format(i, "active"))
+
+	# Väntar på att övergången till sliden blir klar
+	time.sleep(0.5)
+
+	# Sparar en skärmbild av sliden
+	driver.save_screenshot(str.format("screenshots/slide_{}.png", i))
+
+# Stänger webbläsaren
+driver.close()
