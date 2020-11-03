6e6495d780336f2235773214e5ac6266520327b2
Joakim Kokocha <joakim.kokocha@elev.ntig.se>
2020-11-03 13:36:53 +0100

    test now checks if data changes successfully

--- tests/test_modify_data.py
+++ tests/test_modify_data.py
@@ -1,6 +1,7 @@
 import gspread
 import subprocess
 import time
+import os
 
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as options
@@ -8,23 +9,30 @@ from selenium.webdriver.firefox.options import Options as options
 ops = options()
 ops.headless = True
 
+driver = webdriver.Firefox(executable_path="tests/geckodriver", options=ops)
+
 # Connects to service account
 gc = gspread.service_account()
-
 # Opens śpreadhseet by ID
 sh = gc.open_by_key("1k0qCUQbKvipCa8dhFcFjccRAWVGSeYF_MJwcu1Fy5Ls")
-
 # Opens specific page on spreadsheet
-proc = sh.get_worksheet(1)
-
+procivitas = sh.get_worksheet(1)
 # Opens specific cell
-cell = proc.acell("H3").value
-
+cell = procivitas.acell("H3").value
 # Updates cell
-change = proc.update("H3", cell + "s")
+change = procivitas.update("H3", cell + "s")
+newcell = procivitas.acell("H3").value
 
 # Runs get_csv.sh
 subprocess.call(["sh", "get_csv.sh"])
 
+# Opens website instance
+driver.get("http://127.0.0.1:4000/")
 
+pagesource = driver.page_source
 
+# Tries to find the new cell value on the live page
+if newcell in pagesource:
+    print("Data changed on page successfully!")
+else:
+    print("ERROR: Test failed")
