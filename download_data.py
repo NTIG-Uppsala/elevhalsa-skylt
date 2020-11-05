3acae088fcfd3e845477a7702dc7c9fa3c4cac91
Pluppen <eliass.juremalm@gmail.com>
2020-11-05 15:02:04 +0100

    Fix typo in download data

--- download_data.py
+++ download_data.py
@@ -4,5 +4,5 @@ import pathlib
 
 PATH = pathlib.Path(__file__).parent.absolute()
 
-subprocess.call(["sh", f"{PATH}get_csv.sh", "--no-refresh"])
-subprocess.call(["python3", f"{PATH}get_images.py"])
+subprocess.call(["sh", f"{PATH}/get_csv.sh", "--no-refresh"])
+subprocess.call(["python3", f"{PATH}/get_images.py"])
