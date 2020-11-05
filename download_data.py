a189ff93656c3f9047c6bf7e8865007d35b19323
Pluppen <eliass.juremalm@gmail.com>
2020-11-05 14:59:36 +0100

    Possible fix for pi error

--- download_data.py
+++ download_data.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 import subprocess
+import pathlib
 
-subprocess.call(["sh", "get_csv.sh", "--no-refresh"])
-subprocess.call(["python3", "get_images.py"])
+PATH = pathlib.Path(__file__).parent.absolute()
+
+subprocess.call(["sh", f"{PATH}get_csv.sh", "--no-refresh"])
+subprocess.call(["python3", f"{PATH}get_images.py"])
