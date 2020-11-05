71629884d1e0fe64ea6f079a57a8ecb4c69e5ac1
Pluppen <eliass.juremalm@gmail.com>
2020-11-05 14:12:50 +0100

    Removed the loop

--- download_data.py
+++ download_data.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-import time, subprocess
+import subprocess
 
-MINUTES_TO_WAIT = 10
-
-while True:
-    subprocess.call(["sh", "get_csv.sh"])
-    subprocess.call(["python3", "get_images.py"])
-    time.sleep(1000*60*MINUTES_TO_WAIT)
+subprocess.call(["sh", "get_csv.sh"])
+subprocess.call(["python3", "get_images.py"])
