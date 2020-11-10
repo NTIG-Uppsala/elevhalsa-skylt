aec5d61a5d8db8cdf8d9b198dd557e0f997d35d7
Pluppen <eliass.juremalm@gmail.com>
2020-11-10 13:10:44 +0100

    Increase download intervall

--- download_data.py
+++ download_data.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-import subprocess
+import subprocess, time
 import pathlib
 
 PATH = pathlib.Path(__file__).parent.absolute()
 
 subprocess.call(["sh", f"{PATH}/get_csv.sh", "--no-refresh"])
 subprocess.call(["python3", f"{PATH}/get_images.py"])
+
+while True:
+    subprocess.call(["sh", f"{PATH}/get_csv.sh"])
+    time.sleep(60*5)
