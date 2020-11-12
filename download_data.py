727ca4ead396c7b4419e281c0f1d93a1e87a73f9
Pluppen <eliass.juremalm@gmail.com>
2020-11-12 11:32:40 +0100

    Update downlaod_data to run parse_salary.py in the loop

--- download_data.py
+++ download_data.py
@@ -10,4 +10,5 @@ subprocess.call(["python3", f"{PATH}/parse_salary.py"])
 
 while True:
     subprocess.call(["sh", f"{PATH}/get_csv.sh"])
+    subprocess.call(["python3", f"{PATH}/parse_salary.py"])
     time.sleep(60*5)
