32d75dfaa47cc11aabf219e95bec69427ab9c7db
Joakim Kokocha <joakim.kokocha@elev.ntig.se>
2021-02-05 13:37:50 +0100

    changed update sequence in download data

--- download_data.py
+++ download_data.py
@@ -4,11 +4,9 @@ import pathlib
 
 PATH = pathlib.Path(__file__).parent.absolute()
 
-subprocess.call(["sh", f"{PATH}/get_csv.sh"])
 subprocess.call(["python3", f"{PATH}/get_images.py"])
-subprocess.call(["python3", f"{PATH}/parse_salary.py"])
 
 while True:
-    time.sleep(60*5)
-    subprocess.call(["sh", f"{PATH}/get_csv.sh", "--not-refresh"])
+    subprocess.call(["sh", f"{PATH}/get_csv.sh"])
     subprocess.call(["python3", f"{PATH}/parse_salary.py"])
+    time.sleep(60*5)
