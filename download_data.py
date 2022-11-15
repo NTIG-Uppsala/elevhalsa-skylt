08e0a26bb365e434467a30ebd8b36e057ed63fd1
erihedDev <erik.hedlund@elev.ga.ntig.se>
2022-11-15 10:52:10 +0100

    Updated to current version of pi
    Co-Authored-By: Axelgustavschnurer <111983315+Axelgustavschnurer@users.noreply.github.com>
    Co-Authored-By: VincentKarm <105645043+VincentKarm@users.noreply.github.com>
    Co-Authored-By: Hilmir Brynjolfsson <111963441+Hilmir4@users.noreply.github.com>

--- download_data.py
+++ download_data.py
@@ -5,6 +5,6 @@ import pathlib
 PATH = pathlib.Path(__file__).parent.absolute()
 
 while True:
-    subprocess.call(["sh", f"{PATH}/get_csv.sh"])
+    subprocess.call(["python3", f"{PATH}/get_csv.py"])
     subprocess.call(["python3", f"{PATH}/get_images.py"])
     time.sleep(5)
---
+++ get_csv.py
@@ -0,0 +1,42 @@
+import csv
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import subprocess
+import time
+
+# This script uses google service accounts to authorize with the spreadsheet containing data, 
+# (https://robocorp.com/docs/development-guide/google-sheets/interacting-with-google-sheets)
+
+# URLs the service account uses to authorize to google spreadsheets
+scope = ['https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive"]
+
+# Finds the json file with credentials for the service account, and authorizes the service account to gspread
+credentials = ServiceAccountCredentials.from_json_keyfile_name('/home/pi/Git/elevhalsa-skylt/client_login.json', scope)
+client = gspread.authorize(credentials)
+
+# Opens the spreadsheet containing data and gets all the values from the first index (page) of the spreadsheet
+sh = client.open_by_key("1qY1KYAY-AjFh2DWsjiVwOVj2qqJ29kpSs_YaBHi-TEs")
+rows = sh.get_worksheet(0).get_all_values()
+
+# Opens the current csv file with data, then reads and saves every row in a list
+with open("/home/pi/Git/elevhalsa-skylt/site/_data/stored_data.csv", 'r', encoding="utf-8") as r:
+    currentSheet = []
+    csvreader = csv.reader(r)
+    for row in csvreader:
+        currentSheet.append(row)
+
+# Compares currently saved csv data with the data on the spreadsheet, and updates it if changes has been made
+if currentSheet != rows:
+    print("Data changed, updating...")
+    with open("/home/pi/Git/elevhalsa-skylt/site/_data/stored_data.csv", "w", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        for row in rows:
+            writer.writerow(row)
+    print("Data has been updated, refreshing page!")
+
+    # Refreshes the page after two seconds if change has been found (delay is for syncing reasons)
+    time.sleep(2)
+    subprocess.run(["xdotool", "key", "F5"])
+
+
