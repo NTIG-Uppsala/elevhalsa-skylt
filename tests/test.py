515de8ccc1fe30162122c7f447330259d3fe5690
EstefanRK <142974977+EstefanRK@users.noreply.github.com>
2023-10-11 14:05:45 +0200

    Created python file for tests

---
+++ tests/test.py
@@ -0,0 +1,35 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class TestLocalhostPageTitle(unittest.TestCase):
+    # Executes before each test
+    def setUp(self):
+        # Set up the Chrome WebDriver
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        # Load page
+        self.driver.get("http://127.0.0.1:4000")
+        time.sleep(1)
+
+    def test_page_title(self):
+        self.assertEqual("EHT-skylt", self.driver.title)
+
+    def tearDown(self):
+        # Close the WebDriver
+        self.driver.quit()
+
+
+class TestLocalScripts(unittest.TestCase):
+    # Executes before each test
+    def setUp(self):
+        pass
+
+    def test_get_csv(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
