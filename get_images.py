126be97877208ae4e2c8441a7b8cc0bd7f9f2ea2
Aprizotec <65119813+Johannes29@users.noreply.github.com>
2023-10-11 10:37:21 +0200

    Fix get_images.py
    It now actually downloads images

--- get_images.py
+++ get_images.py
@@ -1,37 +1,39 @@
 #!/usr/bin/python3
-import subprocess
-import wget, os, openpyxl, pathlib
+from oauth2client.service_account import ServiceAccountCredentials
+import openpyxl, pathlib
 from openpyxl_image_loader import SheetImageLoader
-from PIL import Image, ImageChops
-import time
-import datetime
+import requests
 
-updateTime = datetime.datetime.now().strftime('%H:%M')
+SPREADSHEET_ID = "1qY1KYAY-AjFh2DWsjiVwOVj2qqJ29kpSs_YaBHi-TEs"
+CREDENTIALS_JSON_FILE = "client_login.json"
 
-PATH = pathlib.Path(__file__).parent.absolute()
-
-img_path = f"{PATH}/site/assets/img"
+file_path = pathlib.Path(__file__).parent.absolute()
+img_path = f"{file_path}/site/assets/img"
 avatar_img_path = img_path + "/avatar.jpg"
 profile_img_path = img_path + "/Profile/{}.jpg"
 
-filename = wget.download("https://docs.google.com/spreadsheets/d/1qY1KYAY-AjFh2DWsjiVwOVj2qqJ29kpSs_YaBHi-TEs/export?format=xlsx")
-pxl_doc = openpyxl.load_workbook(filename)
+
+scope = ['https://www.googleapis.com/auth/spreadsheets',
+         "https://www.googleapis.com/auth/drive"]
+credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_JSON_FILE, scope)
+# from https://stackoverflow.com/a/69776579
+access_token = credentials.create_delegated(credentials._service_account_email).get_access_token().access_token
+url = "https://www.googleapis.com/drive/v3/files/" + spreadsheet_id + "/export?mimeType=application%2Fvnd.openxmlformats-officedocument.spreadsheetml.sheet"
+res = requests.get(url, headers={"Authorization": "Bearer " + access_token})
+
+with open("info.xlsx", 'wb') as f:
+    f.write(res.content)
+
+exel_document = openpyxl.load_workbook("info.xlsx")
 
 def has_changed(sheet):
-
     image_loader = SheetImageLoader(sheet)
-    changed = False
 
     for row in range(2, sheet.max_row + 1):
         image_filename = sheet["K" + str(row)].value
         image_path = profile_img_path.format(image_filename)
         image = image_loader.get("I" + str(row))
         image.save(image_path)
-        changed = True
-        return changed
 
 if __name__ == "__main__":
-    if updateTime == "17:00":
-        has_changed(pxl_doc["NTI"])
-        subprocess.run(["xdotool", "key", "F5"])
-        time.sleep(60)
+    has_changed(exel_document["NTI"])
