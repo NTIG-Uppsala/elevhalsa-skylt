d8ad867aae93205194b19c4d66a75120d5a79b37
Aprizotec <65119813+Johannes29@users.noreply.github.com>
2023-10-11 11:24:59 +0200

    Refactor get_images.py
    And fix spreadsheet_id variable name

--- get_images.py
+++ get_images.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from oauth2client.service_account import ServiceAccountCredentials
-import openpyxl, pathlib
+import openpyxl
 from openpyxl_image_loader import SheetImageLoader
+import pathlib
 import requests
 
 SPREADSHEET_ID = "1qY1KYAY-AjFh2DWsjiVwOVj2qqJ29kpSs_YaBHi-TEs"
@@ -9,20 +10,20 @@ CREDENTIALS_JSON_FILE = "client_login.json"
 
 file_path = pathlib.Path(__file__).parent.absolute()
 img_path = f"{file_path}/site/assets/img"
-avatar_img_path = img_path + "/avatar.jpg"
 profile_img_path = img_path + "/Profile/{}.jpg"
-
 
 scope = ['https://www.googleapis.com/auth/spreadsheets',
          "https://www.googleapis.com/auth/drive"]
 credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_JSON_FILE, scope)
 # from https://stackoverflow.com/a/69776579
 access_token = credentials.create_delegated(credentials._service_account_email).get_access_token().access_token
-url = "https://www.googleapis.com/drive/v3/files/" + spreadsheet_id + "/export?mimeType=application%2Fvnd.openxmlformats-officedocument.spreadsheetml.sheet"
+# MIME type from https://developers.google.com/drive/api/guides/ref-export-formats
+mime_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+url = f"https://www.googleapis.com/drive/v3/files/{SPREADSHEET_ID}/export?mimeType={mime_type}"
 res = requests.get(url, headers={"Authorization": "Bearer " + access_token})
 
-with open("info.xlsx", 'wb') as f:
-    f.write(res.content)
+with open("info.xlsx", 'wb') as file:
+    file.write(res.content)
 
 exel_document = openpyxl.load_workbook("info.xlsx")
 
