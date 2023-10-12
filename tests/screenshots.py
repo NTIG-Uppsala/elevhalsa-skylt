abfbefbd56f92d6dd365e8473430b0a91b252805
Aprizotec <65119813+Johannes29@users.noreply.github.com>
2023-10-12 14:33:36 +0200

    Remove unnecessary code in schreenshots.py

--- tests/screenshots.py
+++ tests/screenshots.py
@@ -1,8 +1,6 @@
 import unittest
 import time
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
 
 # make sure to have a jekyll server up and running before running the tests
 # (run "jekyll serve -s site" in an Ubuntu terminal while in the repo directory)
@@ -12,7 +10,7 @@ class TestLocalhostPageTitle(unittest.TestCase):
     # Executes before each test
     def setUp(self):
         # Set up the Chrome WebDriver
-        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome()
         # Load page
         self.driver.get("http://127.0.0.1:4000")
         time.sleep(1)
