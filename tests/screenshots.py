fde697a15f3af6db188c82734a45439620baa962
EstefanRK <142974977+EstefanRK@users.noreply.github.com>
2023-10-12 13:12:02 +0200

    renamed test for screenshot file

--- tests/screenshots.py
+++ tests/screenshots.py
@@ -3,11 +3,6 @@ import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-import gspread
-import subprocess
-
-from datetime import datetime
-
 
 # make sure to have a jekyll server up and running before running the tests
 # (run "jekyll serve -s site" in an Ubuntu terminal while in the repo directory)
@@ -22,7 +17,7 @@ class TestLocalhostPageTitle(unittest.TestCase):
         self.driver.get("http://127.0.0.1:4000")
         time.sleep(1)
 
-    def test_page_title(self):
+    def test_screenshots(self):
         # take screenshot and save it to a folder
         pass
 
