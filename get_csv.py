e31b6ed6c3d4ee6eb4b2c56a7e61aaa3854cf6db
EstefanRK <142974977+EstefanRK@users.noreply.github.com>
2023-10-16 11:31:50 +0200

    get_csv automatically creates data
    get_csv automatically creates data file if it doesn't exist

--- get_csv.py
+++ get_csv.py
@@ -24,18 +24,28 @@ sh = client.open_by_key("1qY1KYAY-AjFh2DWsjiVwOVj2qqJ29kpSs_YaBHi-TEs")
 rows = sh.get_worksheet(0).get_all_values()
 
 # Opens the current csv file with data, then reads and saves every row in a list
-with open("site/_data/stored_data.csv", "r", encoding="utf-8") as r:
-    current_sheet = []
-    csvreader = csv.reader(r)
-    for row in csvreader:
-        current_sheet.append(row)
+
+csvPath = "site/_data/stored_data.csv"
+try:
+    with open(csvPath, "r", encoding="utf-8") as r:
+        current_sheet = []
+        csvreader = csv.reader(r)
+        for row in csvreader:
+            current_sheet.append(row)
+except:
+    open(csvPath, "x", encoding="utf-8")
+    with open(csvPath, "r", encoding="utf-8") as r:
+        current_sheet = []
+        csvreader = csv.reader(r)
+        for row in csvreader:
+            current_sheet.append(row)
 
 # Compares currently saved csv data with the data on the spreadsheet, and updates it if changes has been made
 if current_sheet != rows:
     # removes empty lists from currentSheet
     current_sheet = [item for item in current_sheet if item != []]
     print("Data changed, updating...")
-    with open("site/_data/stored_data.csv", "w", encoding="utf-8") as f:
+    with open(csvPath, "w", encoding="utf-8") as f:
         writer = csv.writer(f)
         for row in rows:
             writer.writerow(row)
