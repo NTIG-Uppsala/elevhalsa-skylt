7c4bdea5d849799cd0f0200da603678bc59c449c
EstefanRK <142974977+EstefanRK@users.noreply.github.com>
2023-10-17 11:44:49 +0200

    added cli arguments to get_images

--- download_data.py
+++ download_data.py
@@ -6,8 +6,9 @@ PATH = pathlib.Path(__file__).parent.absolute()
 
 sheet_id = "1qY1KYAY-AjFh2DWsjiVwOVj2qqJ29kpSs_YaBHi-TEs"
 data_path = "site/_data/stored_data.csv"
+picture_path = "site/assets/img"
 
 while True:
-    subprocess.call(["python3", f"{PATH}/get_images.py"])
+    subprocess.call(["python3", f"{PATH}/get_images.py", sheet_id, picture_path])
     subprocess.call(["python3", f"{PATH}/get_csv.py", sheet_id, data_path])
     time.sleep(5)
