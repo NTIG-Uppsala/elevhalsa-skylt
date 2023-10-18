575f58069413bfa79c9d01f60d38aedd2bebd208
Johannes <65119813+Johannes29@users.noreply.github.com>
2023-10-18 09:12:15 +0200

    Revert "Get images tests"

--- download_data.py
+++ download_data.py
@@ -6,7 +6,7 @@ PATH = pathlib.Path(__file__).parent.absolute()
 
 sheet_id = "1qY1KYAY-AjFh2DWsjiVwOVj2qqJ29kpSs_YaBHi-TEs"
 data_path = "site/_data/stored_data.csv"
-picture_path = "site/assets/img/Profile"
+picture_path = "site/assets/img"
 
 while True:
     subprocess.call(["python3", f"{PATH}/get_images.py", sheet_id, picture_path])
