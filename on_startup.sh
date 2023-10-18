d195ef5f906169283b80bcac970792ef96d4b439
Aprizotec <65119813+Johannes29@users.noreply.github.com>
2023-10-18 10:59:30 +0200

    Add comment explaining logging in on_startup.sh

--- on_startup.sh
+++ on_startup.sh
@@ -3,4 +3,5 @@
 cd /home/pi/Git/elevhalsa-skylt/
 python3 download_data.py &
 chromium-browser --force-device-scale-factor=0.6 --start-fullscreen --kiosk http://127.0.0.1:4000/ --incognito
+# Logs errors and output of all commands to /home/pi/elevhalsa-skylt-log.txt
 ) >> /home/pi/elevhalsa-skylt-log.txt 2>&1
