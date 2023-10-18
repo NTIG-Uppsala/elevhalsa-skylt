4c2e9aade9a907a9bbd3745ec29dd15122dfbd90
ViggoGitHub <viggoks04@gmail.com>
2023-10-18 09:29:39 +0200

    Move js to separate file

---
+++ site/update_status.js
@@ -0,0 +1,38 @@
+$('.carousel').carousel({
+    pause: "false"
+});
+
+let names = document.getElementsByClassName('name');
+let status = document.getElementsByClassName('status');
+let latestChanges = document.getElementsByClassName('latestChange');
+moment.locale('sv');
+
+function updateDisplayedInfo(names, status, latestChanges) {
+    $.getJSON('https://narvaro.ntig.net/api/get/users', function (data) {
+        for (let i = 0; i < data.length; i++) {
+            for (let x = 0; x < names.length; x++) {
+                if (data[i].name == names[x].innerHTML) {
+                    // Hide elements if status was updated more than 24 hours ago
+                    let showTimeSinceChange = moment(data[i].latest_change).isAfter(moment().subtract(24, "hours"))
+                    let visibility = showTimeSinceChange ? "visible" : "hidden";
+                    latestChanges[x].style.visibility = visibility;
+                    status[x].style.visibility = visibility;
+
+                    latestChanges[x].innerHTML = 'Uppdaterad ' + moment(data[i].latest_change).fromNow();
+                    if (data[i].status == true) {
+                        status[x].innerHTML = '&bull; Tillgänglig';
+                        status[x].style.color = "#52FF42"
+                    } else {
+                        status[x].innerHTML = '&bull; Inte tillgänglig'
+                        status[x].style.color = "red"
+                    }
+                }
+            }
+        }
+    });
+}
+
+updateDisplayedInfo(names, status, latestChanges);
+setInterval(() => {
+    updateDisplayedInfo(names, status, latestChanges);
+}, 60000);
