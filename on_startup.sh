7d272deceb2afab4da0bc9873faa6a7c316d58cc
Georg af Wåhlberg <105862332+Georg-af-W@users.noreply.github.com>
2023-10-23 09:57:27 +0200

    Comment on startup

--- on_startup.sh
+++ on_startup.sh
@@ -5,4 +5,5 @@ cd /home/pi/Git/elevhalsa-skylt/
 python3 download_data.py &
 chromium-browser --force-device-scale-factor=0.8 --start-fullscreen --kiosk http://127.0.0.1:4000/ --incognito
 # Logs errors and output of all commands to /home/pi/elevhalsa-skylt-log.txt
+# The purpose of this file is to log errors so the source of an error can be located if something goes wrong.
 ) >> /home/pi/elevhalsa-skylt-log.txt 2>&1
