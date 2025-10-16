b5cdad4c6198947106cd02a564e19e9bd4f99ac6
ProgrammingFrog <pontus.noaksson@elev.ntig.se>
2025-10-16 10:05:47 +0200

    python image save instead of jekyll save
    did it so python file get_images.py saves and copies images to site which then copies to _site.

--- scripts/get_images.py
+++ scripts/get_images.py
@@ -1,23 +1,24 @@
 import csv
 import os
+import shutil
 import openpyxl
 import requests
 from credentials import get_access_token
 from openpyxl_image_loader import SheetImageLoader
 
 def get_images(sheet_id, csv_data_path):
-    IMAGES_FOLDER_PATH = "./site/profilepics" 
+    SITE_PROFILEPICS_PATH = "./site/profilepics"
+    SITE_OUTPUT_PATH = "./_site/profilepics"
+
+    os.makedirs(SITE_PROFILEPICS_PATH, exist_ok=True)
+    os.makedirs(SITE_OUTPUT_PATH, exist_ok=True)
 
     access_token = get_access_token()
     # MIME type from https://developers.google.com/drive/api/guides/ref-export-formats
     mime_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     url = f"https://www.googleapis.com/drive/v3/files/{sheet_id}/export?mimeType={mime_type}"
-
-    if not os.path.exists(IMAGES_FOLDER_PATH):
-        os.makedirs(IMAGES_FOLDER_PATH)
-
     response = requests.get(url, headers={"Authorization": "Bearer " + access_token})
-
+    
     # This file is opened in binary write mode ("wb") because the response from Google API is in binary.
     # Read more about this at https://developers.google.com/drive/api/reference/rest/v3/files/export
     with open("info.xlsx", "wb") as file:
@@ -41,10 +42,15 @@ def get_images(sheet_id, csv_data_path):
 
     for row in range(2, excel_spreadsheet.max_row + 1):
         image_filename = stored_csv_data[row - 1][image_filename_column_index]
-        image_path = os.path.join(IMAGES_FOLDER_PATH, image_filename)
+        image_path = os.path.join(SITE_PROFILEPICS_PATH, image_filename)
         # Create image cell name from column and row numbers
         image_cell_name = chr(ord("A") + image_column_number - 1) + str(row)
         image = image_loader.get(image_cell_name)
         image.save(image_path)
 
     os.remove("info.xlsx")
+
+    for filename in os.listdir(SITE_PROFILEPICS_PATH):
+        src = os.path.join(SITE_PROFILEPICS_PATH, filename)
+        dst = os.path.join(SITE_OUTPUT_PATH, filename)
+        shutil.copy2(src, dst)
